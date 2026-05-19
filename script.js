b0bf37871fa67f41094e4cbbc8afc15c51252a5d
mohamadabriansah-coder <mohamadabriansah@student.ppns.ac.id>
2026-05-20 00:02:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ detectBtn.addEventListener("click", async () => {
     form.append("file", fileInput.files[0]);
 
     // Confidence threshold
-    form.append("conf", "0.05");
+    form.append("conf", "0.01");
 
     // IoU threshold
     form.append("iou", "0.7");
